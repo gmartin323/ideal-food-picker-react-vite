e57911a965888684789b322dfc932c38a788a8f3
gmartin323 <116142265+gmartin323@users.noreply.github.com>
2023-07-17 15:41:47 +0700

    initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App() {
+  return (
+      <div className="App">
+        <h1>Ideal Food Picker <i className="fa-solid fa-cutlery"></i></h1>
+      </div>
+  );
+}
+
+// this is the same as App.js in CRA
