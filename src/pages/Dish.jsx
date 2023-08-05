7bfe8ae12b06eb6a5f1d4a6fefce4ac894074195
gmartin323 <116142265+gmartin323@users.noreply.github.com>
2023-08-05 14:04:29 +0700

    CSS dish page adjustments fro larger screens

--- src/pages/Dish.jsx
+++ src/pages/Dish.jsx
@@ -8,37 +8,43 @@ export default function Dish(props) {
   }
   
   const imgSrc = `src/images/dish/${props.suggestedDish.img}`
+  const restaurantName = `at \n ${props.suggestedDish.restaurant} `
 
   return (
-    <div className="dish-card">
-      <div className="dish-container">
-        <img
-          className="dish-img"
-          src={imgSrc}
-          // src={`images/dish/${props.suggestedDish.img}`}
-        />
-        <h1 className="dish-name">{props.suggestedDish.name}</h1>
+    <div>
+      <div className="dish-card">
+        <div className="dish-container">
+          <img
+            className="dish-img"
+            src={imgSrc}
+            // src={`images/dish/${props.suggestedDish.img}`}
+          />
+          <h1 className="dish-name">{props.suggestedDish.name}</h1>
+          <p className="dish-price">{props.suggestedDish.price}₫</p>
+        </div>
+        <div className="restaurant-container">
+          <p className="restaurant-name new-line">
+            {restaurantName}
+          </p>
+          <div className="restaurant-map">
+            <iframe 
+              src={props.suggestedDish.location}
+              width="100%"
+              height= "80%"
+              style={
+                {borderRadius: "8px",
+                borderStyle: "none"}
+              }
+              allowFullScreen="" 
+              loading="lazy" 
+              referrerPolicy="no-referrer-when-downgrade"> 
+            </iframe>
+          </div>  
+          
+          <p className="restaurant-address">{props.suggestedDish.address}</p>
+        </div>
       </div>
-      <div className="restaurant-container">
-        <span className="restaurant-name">{props.suggestedDish.restaurant}</span>
-        <div className="restaurant-map">
-          <iframe 
-            src={props.suggestedDish.location}
-            width="100%"
-            height= "80%"
-            style={
-              {borderRadius: "8px",
-              borderStyle: "none"}
-            }
-            allowFullScreen="" 
-            loading="lazy" 
-            referrerPolicy="no-referrer-when-downgrade"> 
-          </iframe>
-        </div>  
-        <p className="dish-price">{props.suggestedDish.price}₫</p>
-        <p className="restaurant-address">{props.suggestedDish.address}</p>
-        <button className="restart-btn" onClick={refreshPage}>Start Again</button>
-      </div>
+      <button className="restart-btn" onClick={refreshPage}>Start Again</button>
     </div>
   )
 }
